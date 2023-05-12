340203358f6e9eae1aca8be8578f0cb7ce394db9
Daniel Hauck <mail@dhauck.eu>
2023-05-12 13:26:55 +0200

    Make host and port configurable.

--- src/extension.ts
+++ src/extension.ts
@@ -7,30 +7,45 @@ export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
     "zotero-for-typst.pickCitation",
     () => {
+      let config = vscode.workspace.getConfiguration("zoteroForTypst");
+      let host = config.get("host");
+      let port = config.get("port");
       let url =
-        "http://127.0.0.1:23119/better-bibtex/cayw?format=playground" +
+        `http://${host}:${port}/better-bibtex/cayw?format=playground` +
         `&citeprefix=${encodeURIComponent("#cite(")}` +
         `&citepostfix=${encodeURIComponent(")")}` +
         `&keyprefix=${encodeURIComponent('"')}` +
         `&keypostfix=${encodeURIComponent('"')}` +
         `&separator=${encodeURIComponent(", ")}`;
       console.log(url);
-      http.get(url, (res) => {
-        let data = "";
+      http
+        .get(url, (res) => {
+          let data = "";
 
-        res.on("data", (chunk) => {
-          data += chunk;
-        });
+          res.on("data", (chunk) => {
+            data += chunk;
+          });
 
-        res.on("end", () => {
-          let editor = vscode.window.activeTextEditor;
-          editor.edit((edit) =>
-            editor.selections.forEach((selection) =>
-              edit.replace(selection, data)
+          res.on("end", () => {
+            let editor = vscode.window.activeTextEditor;
+            editor.edit((edit) =>
+              editor.selections.forEach((selection) =>
+                edit.replace(selection, data)
+              )
+            );
+          });
+
+          res.on("error", (error) =>
+            vscode.window.showErrorMessage(
+              `Could not fetch citations: ${error.message}`
             )
           );
-        });
-      });
+        })
+        .on("error", (error) =>
+          vscode.window.showErrorMessage(
+            `Could not fetch citations: ${error.message}`
+          )
+        );
     }
   );
 
